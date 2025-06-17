257aa62de73fd110162e7da8715b6c4918cd1441
Sebastian Huertas <shuertas567@gmail.com>
2025-06-17 08:20:39 -0500

    Cambios 17/06 8:20

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -5,6 +5,10 @@ use Illuminate\Support\Facades\Redirect;
 use App\Models\Product;
 use App\Http\Controllers\CategoriaController;
 use App\Http\Controllers\ProductController; 
+use App\Http\Controllers\AdminController;
+use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\CarritoController;
+
 
 Route::get('/inicio', function () {
     $products = Product::with('categoria')->get();
@@ -35,3 +39,6 @@ Route::get('/categorias/id/{id}', [CategoriaController::class, 'mostrarPorId'])-
 Route::get('/producto/{id}', [ProductController::class, 'mostrar'])->name('producto.ver');
 
 Route::get('/buscar', [ProductController::class, 'buscar'])->name('buscar.productos');
+Route::middleware(['auth', 'admin'])->group(function () {
+    Route::get('/admin', [AdminController::class, 'dashboard'])->name('admin.dashboard');
+});
