7ff21f9ee62ae9783119493318c41e4bc3c23456
Sebastian Huertas <shuertas567@gmail.com>
2025-07-01 03:48:44 -0500

    Cambios de front end y añadir pseudo boton de mis pedidos

--- backend/resources/views/layouts/plantilla.blade.php
+++ backend/resources/views/layouts/plantilla.blade.php
@@ -5,19 +5,29 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>@yield('title', 'I LIKE YOU')</title>
     <link rel="stylesheet" href="{{ asset('css/estilos.css') }}">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" defer></script>
+<link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;600&display=swap" rel="stylesheet">
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.css">
+
+
+
+
 </head>
 <body>
     {{-- HEADER --}}
     @include('partials.header')
-    
-    <div class="container">
+
+    <div class="d-flex">
         {{-- SIDEBAR --}}
         @include('partials.sidebar')
 
         {{-- CONTENIDO PRINCIPAL --}}
-        <main>
+        <main class="flex-grow-1 p-4">
             @yield('content')
         </main>
     </div>
 </body>
+
+
 </html>
