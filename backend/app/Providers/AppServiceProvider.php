925617c726bdfe9ace0dc3f511f667dabf9db503
Alvaro <alvaro25pro@gmail.com>
2025-07-14 12:17:23 -0500

    Beta

--- backend/app/Providers/AppServiceProvider.php
+++ backend/app/Providers/AppServiceProvider.php
@@ -25,6 +25,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
 {
+                if (env('APP_ENV') === 'production') {
+        URL::forceScheme('https');
+    }
+        Paginator::useBootstrap();
     // Compartir categorías con todas las vistas automáticamente
     View::share('categorias', Categoria::all());
 }
